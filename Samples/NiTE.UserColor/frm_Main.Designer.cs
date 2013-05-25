157df3b125c564b053de7f320fb1e04932c45766
falahati <s_falahati@hotmail.com>
2013-05-25 23:07:25 +0430

    User color sample added for NiTE

---
+++ Samples/NiTE.UserColor/frm_Main.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace NiTEUserColor
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_preview = new System.Windows.Forms.PictureBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_preview
+            // 
+            this.pb_preview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_preview.Location = new System.Drawing.Point(12, 12);
+            this.pb_preview.Name = "pb_preview";
+            this.pb_preview.Size = new System.Drawing.Size(640, 480);
+            this.pb_preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_preview.TabIndex = 4;
+            this.pb_preview.TabStop = false;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(658, 468);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // frm_Main
+            // 
+            this.AcceptButton = this.btn_start;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 502);
+            this.Controls.Add(this.pb_preview);
+            this.Controls.Add(this.btn_start);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Main";
+            this.ShowIcon = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frm_Main_FormClosing);
+            this.Load += new System.EventHandler(this.frm_Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_preview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_preview;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ Samples/NiTE.UserColor/frm_Main.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NiTEWrapper;
+using System.Drawing.Imaging;
+
+namespace NiTEUserColor
+{
+    public partial class frm_Main : Form
+    {
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        static bool HandleError(NiTE.Status status)
+        {
+            if (status == NiTE.Status.OK)
+                return true;
+            MessageBox.Show("Error: " + status.ToString() + Environment.NewLine + OpenNIWrapper.OpenNI.LastError);
+            return false;
+        }
+
+        private void frm_Main_Load(object sender, EventArgs e)
+        {
+            if (HandleError(NiTE.Initialize()))
+            {
+                this.Text = "Running by NiTE v" + NiTE.Version.ToString();
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+        }
+        UserTracker uTracker;
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            uTracker = UserTracker.Create();
+            btn_start.Enabled = false;
+            //uTracker.onNewData += new UserTracker.UserTrackerListener(uTracker_onNewData);
+
+            /* Because of incompatibility between current version of OpenNI and NiTE,
+             * we can't use event based reading. So we put our sample in a loop.
+             * You can copy OpenNI.dll from version 2.0 to solve this problem.
+             * Then you can uncomment above line of code and comment below ones.
+             */
+            while (true)
+            {
+                uTracker_onNewData(uTracker);
+                Application.DoEvents();
+            }
+        }
+
+        ulong lastTime = 0;
+        ulong fps;
+        void uTracker_onNewData(UserTracker uTracker)
+        {
+            using (UserTrackerFrameRef frame = uTracker.readFrame())
+            {
+                UserMap um = frame.UserMap;
+                FillImageFromUserMap(frame.UserMap);
+
+                UserData[] ud = frame.Users;
+                using (Graphics g = Graphics.FromImage(image))
+                {
+                    foreach (UserData user in frame.Users)
+                    {
+                        if (user.CenterOfMass.Z > 0)
+                        {
+                            Point p = new Point();
+                            PointF pf = uTracker.ConvertJointCoordinatesToDepth(user.CenterOfMass);
+                            p.X = (int)pf.X - 5;
+                            p.Y = (int)pf.Y - 5;
+                            g.DrawEllipse(new Pen(Brushes.White, 5), new Rectangle(p, new Size(5, 5)));
+                            g.DrawString("Center Of Mass", SystemFonts.DefaultFont, Brushes.White, p.X - 40, p.Y - 20);
+                        }
+                    }
+                    g.Save();
+                }
+
+                this.Invoke(new Action(delegate()
+                {
+                    fps = ((1000000 / (frame.Timestamp - lastTime)) + (fps * 4)) / 5;
+                    lastTime = frame.Timestamp;
+                    this.Text = "Frame #" + frame.FrameIndex.ToString() + " - Time: " + frame.Timestamp.ToString() + " - FPS: " + fps.ToString();
+                    pb_preview.Image = image.Clone(new Rectangle(new Point(0,0), image.Size), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                }));
+                
+            }
+        }
+
+        Bitmap image;
+        private unsafe void FillImageFromUserMap(UserMap um)
+        {
+            int[] colors = new int[] { 16777215, 14565387, 32255, 7996159, 16530175, 8373026, 14590399, 7062435, 13951499, 55807 };
+            if (image == null || image.Width != um.FrameSize.Width || image.Height != um.FrameSize.Height)
+                image = new Bitmap(um.FrameSize.Width, um.FrameSize.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            BitmapData bd = image.LockBits(new Rectangle(new Point(0, 0), image.Size), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            for (int y = 0; y < um.FrameSize.Height; y++)
+            {
+                ushort* dataPos = (ushort*)((byte*)um.Pixels.ToPointer() + (y * um.DataStrideBytes));
+                byte* imagePos = (byte*)bd.Scan0.ToPointer() + (y * bd.Stride);
+                for (int x = 0; x < um.FrameSize.Width; x++)
+                {
+                    int color = colors[*dataPos % colors.Length];
+                    *imagePos = (byte)(((color / 1) % 256) * (*dataPos)); // R
+                    imagePos++;
+                    *imagePos = (byte)(((color / 256) % 256) * (*dataPos)); // G
+                    imagePos++;
+                    *imagePos = (byte)(((color / 65536) % 256) * (*dataPos)); // B
+                    imagePos++;
+                    dataPos++;
+                }
+            }
+            image.UnlockBits(bd);
+        }
+
+        private void frm_Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            NiTE.Shutdown();
+        }
+    }
+}
