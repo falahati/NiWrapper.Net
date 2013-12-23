908183f1f7ea6d2e48ce644366d1044a0b162279
falahati <s_falahati@hotmail.com>
2013-12-23 14:40:31 +0330

    Sample added for image registration

---
+++ ConsoleImageRegistration/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleImageRegistration
+{
+    using System;
+    using System.Drawing;
+    using OpenNIWrapper;
+
+    class Program
+    {
+        private static Device device;
+
+        private static VideoStream depthStream, colorStream;
+        static void Main(string[] args)
+        {
+            try
+            {
+                OpenNI.Initialize();
+                device = Device.Open(Device.ANY_DEVICE);
+
+                depthStream = device.CreateVideoStream(Device.SensorType.DEPTH);
+                depthStream.VideoMode = new VideoMode
+                {
+                    DataPixelFormat = VideoMode.PixelFormat.DEPTH_1MM,
+                    FPS = 30,
+                    Resolution = new Size(640, 480)
+                };
+
+                colorStream = device.CreateVideoStream(Device.SensorType.COLOR);
+                colorStream.VideoMode = new VideoMode
+                {
+                    DataPixelFormat = VideoMode.PixelFormat.RGB888,
+                    FPS = 30,
+                    Resolution = new Size(640, 480)
+                };
+                device.DepthColorSyncEnabled = true;
+                depthStream.Start();
+                colorStream.Start();
+                device.ImageRegistration = Device.ImageRegistrationMode.DEPTH_TO_COLOR;
+                Console.WriteLine("Image registration is active and working well.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+            if (device != null)
+                device.Close();
+            OpenNI.Shutdown();
+        }
+    }
+}
---
+++ ConsoleImageRegistration/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleImageRegistration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleImageRegistration")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8fe1c43a-a70f-4623-aa1b-713be26f967e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
